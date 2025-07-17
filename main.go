24245455f1abc98b4b7334b0580cbf494e7f8d59
zer0reaction <boltgopnik@gmail.com>
2025-07-17 12:07:07 +0300

    Tokenizer rewrite

--- main.go
+++ main.go
@@ -4,8 +4,28 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
+	// "strconv"
 	"strings"
+)
+
+// TOKENS
+
+type tokenType uint32
+
+const (
+	ttEmpty tokenType = iota // used for initialization and errors
+	ttBlank
+	ttEOF
+	ttIdentifier
+	ttSemicolon
+	ttBracketOpen
+	ttBracketClose
+	ttEqual
+	ttPlus
+	ttIntLit
+	ttLet
+	ttU32
+	ttCounter
 )
 
 type token struct {
@@ -13,45 +33,29 @@ type token struct {
 	data string
 }
 
-type tokenType uint32
-
-const (
-	ttEmpty tokenType = iota
-	ttSymbol
-	ttEqual
-	ttIntegerLiteral
-	ttCharLiteral
-	ttSemicolon
-	ttPutbyte
-	ttEOF
-	ttCounter
-)
+// NODES
 
 type nodeType uint32
 
 const (
 	ntEmpty nodeType = iota
-	ntRoot
-	ntOpAssign
-	ntOpPutbyte
-	ntVariable
-	ntIntLiteral
-	ntCharLiteral
 	ntCounter
 )
 
-// NOTE: all variables are uint32
 type node struct {
 	t nodeType
-
-	rootChildNodes []node
-	opArgs         []node
-	varName        string
-	intLitValue    uint32
-	charLitValue   uint8
 }
 
+// VARIABLES
+
+type variableType uint32
+
+const (
+	vtU32 variableType = iota
+)
+
 type variable struct {
+	t      variableType
 	name   string
 	offset uint32
 }
@@ -59,7 +63,7 @@ type variable struct {
 func main() {
 	log.SetFlags(0)
 
-	if len(os.Args) != 3 {
+	if len(os.Args) != 2 {
 		log.Fatal("incorrect number of arguments")
 	}
 
@@ -68,422 +72,145 @@ func main() {
 		log.Fatal(err)
 	}
 
-	tokens, err := tokenize(string(contents))
+	ts, err := tokenize(string(contents))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	root, err := parse(tokens)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	code, err := codegen(root)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = os.WriteFile(os.Args[2], []byte(code), 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func tokenize(s string) ([]token, error) {
-	var ts []token
-
-	if ttCounter != 8 {
-		panic("tokenize: not all tokens implemented")
-	}
-
-	for len(s) > 0 {
-		t := token{
-			t:    ttEmpty,
-			data: "",
-		}
-
-		switch {
-		// equal
-		case s[0] == '=':
-			t.t = ttEqual
-			s = s[1:]
-
-		// semicolon
-		case s[0] == ';':
-			t.t = ttSemicolon
-			s = s[1:]
-
-		// putbyte
-		case strings.HasPrefix(s, "putbyte "):
-			t.t = ttPutbyte
-			s = s[len("putbyte "):]
-
-		// integer
-		// NOTE: no checks
-		case s[0] >= '0' && s[0] <= '9':
-			t.t = ttIntegerLiteral
-			for len(s) > 0 && s[0] >= '0' && s[0] <= '9' {
-				t.data += (string)(s[0])
-				s = s[1:]
-			}
-
-		// char
-		// NOTE: no escape characters or checks
-		case len(s) >= 3 && s[0] == '\'' && s[2] == '\'':
-			t.t = ttCharLiteral
-			t.data = string(s[1])
-			s = s[3:]
-
-		// symbol
-		case s[0] >= 'a' && s[0] <= 'z':
-			t.t = ttSymbol
-			for len(s) > 0 && (s[0] >= 'a' && s[0] <= 'z' || s[0] == '_') {
-				t.data += (string)(s[0])
-				s = s[1:]
-			}
-
-		default:
-			s = s[1:]
-		}
-
-		if t.t != ttEmpty {
-			ts = append(ts, t)
-		}
-	}
-
-	ts = append(ts, token{t: ttEOF, data: ""})
-
-	return ts, nil
+	printTokens(ts)
 }
 
 func printTokens(ts []token) {
-	if ttCounter != 7 {
-		panic("printTokens: not all tokens implemented")
+	if ttCounter != 12 {
+		panic("not all tokens implemented")
 	}
 
 	for _, t := range ts {
 		switch t.t {
 		case ttEmpty:
-			fmt.Print("[EMPTY] ")
-		case ttSymbol:
-			fmt.Printf("%s ", t.data)
-		case ttEqual:
-			fmt.Print("[=] ")
-		case ttIntegerLiteral:
-			fmt.Printf("(integer %s) ", t.data)
-		case ttPutbyte:
-			fmt.Print("[putbyte] ")
-		case ttSemicolon:
-			fmt.Print("[;]\n")
+			fmt.Printf("[EMPTY] ")
+		case ttBlank:
+			fmt.Printf("[BLANK] ")
 		case ttEOF:
-			fmt.Print("[EOF]\n")
+			fmt.Printf("[EOF]\n")
+		case ttIdentifier:
+			fmt.Printf("%s ", t.data)
+		case ttSemicolon:
+			fmt.Printf("[;]\n")
+		case ttBracketOpen:
+			fmt.Printf("[(] ")
+		case ttBracketClose:
+			fmt.Printf("[)] ")
+		case ttEqual:
+			fmt.Printf("[=] ")
+		case ttPlus:
+			fmt.Printf("[+] ")
+		case ttIntLit:
+			fmt.Printf("<%s> ", t.data)
+		case ttLet:
+			fmt.Printf("[let] ")
+		case ttU32:
+			fmt.Printf("[u32] ")
+		default:
+			panic("unrecognized token")
 		}
 	}
 }
 
-func parse(ts []token) (node, error) {
-	if ttCounter != 8 {
-		panic("parse: not all tokens implemented")
+func tokenize(s string) ([]token, error) {
+	if ttCounter != 12 {
+		panic("not all tokens implemented")
 	}
-	if ntCounter != 7 {
-		panic("parse: not all nodes implemented")
+
+	var ts []token
+	eof := false
+
+	for !eof {
+		t, left, err := chopToken(s)
+		if err != nil {
+			return nil, err
+		}
+
+		if t.t != ttBlank {
+			ts = append(ts, t)
+		}
+		if t.t == ttEOF {
+			eof = true
+		}
+
+		s = left
+	}
+
+	return ts, nil
+}
+
+func chopToken(s string) (token, string, error) {
+	if ttCounter != 12 {
+		panic("not all tokens implemented")
+	}
+
+	t := token{
+		t:    ttEmpty,
+		data: "",
 	}
 
 	switch {
-	case len(ts) == 1:
-		switch {
-		// variable
-		case ts[0].t == ttSymbol:
-			v := node{
-				t:       ntVariable,
-				varName: ts[0].data,
-			}
-			return v, nil
+	case len(s) == 0:
+		t.t = ttEOF
+		return t, "", nil
 
-		// int literal
-		case ts[0].t == ttIntegerLiteral:
-			val, err := strconv.ParseInt(ts[0].data, 10, 32)
-			if err != nil {
-				return node{t: ntEmpty}, err
-			}
+	// single character tokens
+	case s[0] == ';':
+		t.t = ttSemicolon
+		return t, s[1:], nil
+	case s[0] == '(':
+		t.t = ttBracketOpen
+		return t, s[1:], nil
+	case s[0] == ')':
+		t.t = ttBracketClose
+		return t, s[1:], nil
+	case s[0] == '=':
+		t.t = ttEqual
+		return t, s[1:], nil
+	case s[0] == '+':
+		t.t = ttPlus
+		return t, s[1:], nil
 
-			il := node{
-				t:           ntIntLiteral,
-				intLitValue: uint32(val),
-			}
+	// keywords
+	case strings.HasPrefix(s, "let "):
+		t.t = ttLet
+		return t, s[len("let "):], nil
+	case strings.HasPrefix(s, "u32 "):
+		t.t = ttU32
+		return t, s[len("u32 "):], nil
 
-			return il, nil
-
-		// character literal
-		case ts[0].t == ttCharLiteral:
-			if len(ts[0].data) != 1 {
-				panic("length of char literal != 1")
-			}
-			cl := node{
-				t:            ntCharLiteral,
-				charLitValue: uint8(ts[0].data[0]),
-			}
-			return cl, nil
-
-		default:
-			return node{t: ntEmpty}, fmt.Errorf("unexpected token type while trying to parse 1 token")
+	// literals
+	// NOTE: no checks
+	case s[0] >= '0' && s[0] <= '9':
+		t.t = ttIntLit
+		for len(s) > 0 && (s[0] >= '0' && s[0] <= '9') {
+			t.data += string(s[0])
+			s = s[1:]
 		}
-	case len(ts) == 2:
-		switch {
-		// putbyte
-		case ts[0].t == ttPutbyte:
-			op := node{
-				t: ntOpPutbyte,
-			}
+		return t, s, nil
 
-			arg, err := parse(ts[1:2])
-			if err != nil {
-				return node{t: ntEmpty}, fmt.Errorf("failed to parse arg of 'putbyte'")
-			}
-
-			op.opArgs = append(op.opArgs, arg)
-			return op, nil
-
-		default:
-			return node{t: ntEmpty}, fmt.Errorf("unexpected token types while trying to parse 2 tokens")
+	// identifier
+	case s[0] >= 'a' && s[0] <= 'z':
+		t.t = ttIdentifier
+		for len(s) > 0 && ((s[0] >= 'a' && s[0] <= 'z') || s[0] == '_') {
+			t.data += string(s[0])
+			s = s[1:]
 		}
+		return t, s, nil
 
-	case len(ts) == 3:
-		switch {
-		// var = int or var
-		case ts[1].t == ttEqual:
-			op := node{
-				t: ntOpAssign,
-			}
-
-			left, err := parse(ts[0:1])
-			if err != nil {
-				return node{t: ntEmpty}, fmt.Errorf("failed to parse left side arg of '='")
-			}
-			if left.t != ntVariable {
-				return node{t: ntEmpty}, fmt.Errorf("left side arg of '=' is not a variable")
-			}
-
-			right, err := parse(ts[2:3])
-			if err != nil {
-				return node{t: ntEmpty}, fmt.Errorf("failed to parse right side arg of '='")
-			}
-
-			op.opArgs = append(op.opArgs, left)
-			op.opArgs = append(op.opArgs, right)
-			return op, nil
-
-		default:
-			return node{t: ntEmpty}, fmt.Errorf("unexpected token types while trying to parse 3 tokens")
-		}
-
-	case len(ts) > 3:
-		root := node{
-			t: ntRoot,
-		}
-
-		var accumulated []token
-
-		for len(ts) > 0 {
-			if ts[0].t == ttSemicolon {
-				n, err := parse(accumulated)
-				if err != nil {
-					return node{t: ntEmpty}, err
-				}
-
-				root.rootChildNodes = append(root.rootChildNodes, n)
-				accumulated = nil
-				ts = ts[1:]
-			} else {
-				accumulated = append(accumulated, ts[0])
-				ts = ts[1:]
-			}
-		}
-
-		// EOF left
-		if len(accumulated) != 1 {
-			return node{t: ntEmpty}, fmt.Errorf("not all tokens parsed")
-		}
-
-		return root, nil
+	// blank characters
+	case s[0] == ' ' || s[0] == '\t' || s[0] == '\n':
+		t.t = ttBlank
+		return t, s[1:], nil
 
 	default:
-		fmt.Println(len(ts))
-		panic("unexpected number of tokens")
+		err := fmt.Errorf("unrecognized token starting with %c", s[0])
+		t.t = ttEmpty
+		return t, "", err
 	}
-}
-
-func printAST(n node) {
-	if ntCounter != 6 {
-		panic("printAST: not all nodes implemented")
-	}
-
-	switch n.t {
-	case ntRoot:
-		for _, child := range n.rootChildNodes {
-			printAST(child)
-		}
-
-	case ntOpAssign:
-		fmt.Print("[=]: ")
-		for _, child := range n.opArgs {
-			printAST(child)
-		}
-		fmt.Println("")
-
-	case ntOpPutbyte:
-		fmt.Print("[putbyte]: ")
-		for _, child := range n.opArgs {
-			printAST(child)
-		}
-		fmt.Println("")
-
-	case ntVariable:
-		fmt.Printf("%s ", n.varName)
-
-	case ntIntLiteral:
-		fmt.Printf("%v ", n.intLitValue)
-
-	default:
-		fmt.Print("UNRECOGNIZED ")
-	}
-}
-
-func checkVarExistence(vars []variable, name string) int {
-	for i, v := range vars {
-		if v.name == name {
-			return i
-		}
-	}
-	return -1
-}
-
-func codegen(root node) (string, error) {
-	if ntCounter != 7 {
-		panic("codegen: not all nodes implemented")
-	}
-
-	var code string = `.section .text
-.globl _start
-
-putbyte:
-	pushq	%rbp
-	movq	%rsp, %rbp
-
-	movb	%dil, -1(%rbp)
-
-	movq	$1, %rax
-	movq	$1, %rdi
-	leaq	-1(%rbp), %rsi
-	movq	$1, %rdx
-	syscall
-
-	leave
-	ret
-
-_start:
-	mov %rsp, %rbp
-
-`
-	var globalVars []variable
-	var globalOffset uint32 = 0
-
-	for _, node := range root.rootChildNodes {
-		switch node.t {
-		case ntOpAssign:
-			args := node.opArgs
-
-			// we assume the opposite (it is checked in the parser)
-			if len(args) != 2 {
-				panic("codegen: incorrect number of arguments for '='")
-			}
-			if args[0].t != ntVariable {
-				panic("codegen: left argument of '=' is not a variable")
-			}
-
-			varInd := checkVarExistence(globalVars, args[0].varName)
-
-			if varInd == -1 {
-				globalOffset += 4
-				v := variable{
-					offset: globalOffset,
-					name:   args[0].varName,
-				}
-				globalVars = append(globalVars, v)
-				varInd = len(globalVars) - 1
-			}
-
-			switch args[1].t {
-			case ntIntLiteral:
-				leftVar := globalVars[varInd]
-				code += fmt.Sprintf("\tmovl\t$%d, -%d(%%rbp)\n", args[1].intLitValue, leftVar.offset)
-
-			case ntCharLiteral:
-				leftVar := globalVars[varInd]
-				code += fmt.Sprintf("\tmovl\t$%d, -%d(%%rbp)\n", args[1].charLitValue, leftVar.offset)
-
-			case ntVariable:
-				rightVarInd := checkVarExistence(globalVars, args[1].varName)
-				if rightVarInd == -1 {
-					return "", fmt.Errorf("codegen: trying to assign to a non-existing variable")
-				}
-
-				leftVar := globalVars[varInd]
-				rightVar := globalVars[rightVarInd]
-
-				code += fmt.Sprintf("\tmovl\t-%d(%%rbp), %%eax\n", rightVar.offset)
-				code += fmt.Sprintf("\tmovl\t%%eax, -%d(%%rbp)\n", leftVar.offset)
-
-			default:
-				return "", fmt.Errorf("codegen: incorrect right argument of '='")
-			}
-
-		case ntOpPutbyte:
-			args := node.opArgs
-
-			// we assume the opposite (it is checked in the parser)
-			if len(args) != 1 {
-				panic("codegen: incorrect number of arguments for 'putbyte'")
-			}
-
-			// To make sure:
-			// 1. Stack frames don't collide
-			// 2. The stack is 16 byte aligned
-			rspOffset := globalOffset + (16-globalOffset%16)%16
-
-			code += fmt.Sprintf("\tsubq\t$%d, %%rsp\n", rspOffset)
-
-			switch args[0].t {
-			case ntVariable:
-				varInd := checkVarExistence(globalVars, args[0].varName)
-				if varInd == -1 {
-					return "", fmt.Errorf("codegen: trying to print a non-existing variable")
-				}
-
-				code += fmt.Sprintf("\tmovl\t-%d(%%rbp), %%edi\n", globalVars[varInd].offset)
-				code += "\tcall\tputbyte\n"
-
-			case ntIntLiteral:
-				code += fmt.Sprintf("\tmovl\t$%d, %%edi\n", args[0].intLitValue)
-				code += "\tcall\tputbyte\n"
-
-			case ntCharLiteral:
-				code += fmt.Sprintf("\tmovb\t$%d, %%dil\n", args[0].charLitValue)
-				code += "\tcall\tputbyte\n"
-
-			default:
-				return "", fmt.Errorf("codegen: incorrect argument of 'putbyte'")
-			}
-
-			code += fmt.Sprintf("\taddq\t$%d, %%rsp\n", rspOffset)
-		}
-	}
-
-	code += `
-	movq	$60, %rax
-	movq	$0, %rdi
-	syscall
-`
-
-	return code, nil
 }
